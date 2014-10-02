b8895845b791bc7d11db1b71776e2212b49928e0
gtar69 <chihweichang69@gmail.com>
2014-10-02 16:37:12 +0800

    deploy 2nd

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{ubuntu@54.176.241.54}
-role :web, %w{ubuntu@54.176.241.54}
-role :db,  %w{ubuntu@54.176.241.54}
+role :app, %w{ubuntu@54.69.44.169}
+role :web, %w{ubuntu@54.69.44.169}
+role :db,  %w{ubuntu@54.69.44.169}
 
 
 # Extended Server Syntax
@@ -15,10 +15,10 @@ role :db,  %w{ubuntu@54.176.241.54}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '54.176.241.54', user: 'ubuntu', roles: %w{web app}, my_property: :my_value
+server '54.69.44.169', user: 'ubuntu', roles: %w{web app}, my_property: :my_value
 
 set :ssh_options, {
-  keys: [ File.join(ENV['HOME'], '.ssh/gamestoretest.pem')],
+  keys: [ File.join(ENV['HOME'], 'Desktop/gamestoretest.pem')],
   forward_agent: true
 }
 
